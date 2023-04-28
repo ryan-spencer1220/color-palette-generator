02c1239d280664f0cdc13f0f5ad9c2adce8fff88
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-04-27 19:17:48 -0700

    refactor css and js files

--- app.py
+++ app.py
@@ -7,20 +7,25 @@ config = dotenv_values(".env");
 openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
-  template_folder='templates'
+  template_folder='templates',
+  static_url_path='',
+  static_folder='static',
 )
 
 def get_colors(msg):
     prompt = f"""
     You are a color palette generating assistant that responds to text prompts for color palettes
     You should generate color palettes that fit the the theme, mood, or instructions in the prompt.
-    The palettes should be between 2 and 8 colors.
+    The palettes should be between 2 and 6 colors.
 
     Q: Convert the following verbal description of a color palette into a list of colors: Beautiful Sunset
     A: ["#C34F63","#E0845E","#EEBE9F","#F4E4AA","#F7CDB0","#F2AFAC","#E68AAE","#CB6BC8"]
 
     Q: Convert the following verbal description of a color palette into a list of colors: Google Brand Colors
     A: ["#4285F4","#DB4437","#F4B400","#0F9D58"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: Microsoft Brand Colors
+    A: ["#F25022","#7FBA00","#00A4EF","#FFB900", "#737373"]
 
     Desired Format: JSON array of hexadecimal color codes
 
